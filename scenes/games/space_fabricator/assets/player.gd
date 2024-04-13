fb87ab7ba395e7e497cb6e0474acecd7111c3ff6
Ben Eng <whysguy52@gmail.com>
2024-04-13 14:47:47 -0500

    character pointing semi-working

--- scenes/games/space_fabricator/assets/player.gd
+++ scenes/games/space_fabricator/assets/player.gd
@@ -5,7 +5,7 @@ const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 var playerDir
 var isFiring
-var RAY_LENGTH = 1
+var RAY_LENGTH = 40
 var camera
 var rayObj
 # Get the gravity from the project settings to be synced with RigidBody nodes.
@@ -13,9 +13,8 @@ var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 func _ready():
   playerDir = get_node("player_model")
-  camera = get_parent().get_node("Camera3D")
-  rayObj = camera.get_node("ShootTarget")
-  print(rayObj)
+  camera = get_node("Camera3D")
+  rayObj = get_parent().get_node("ShootTarget")
 
 func _process(delta):
   if(Input.is_action_pressed("LMB")):
@@ -49,9 +48,22 @@ func _physics_process(delta):
   move_and_slide()
 
 func point_character(direction:Vector3):
+
   if (isFiring):
     var space_state = get_world_3d().direct_space_state
     var from = camera.project_ray_origin(get_viewport().get_mouse_position())
-    var to = from + camera.project_ray_normal(get_viewport().get_mouse_position())
-  elif(direction.length() != 0):
+    var to = from + camera.project_ray_normal(get_viewport().get_mouse_position()) * RAY_LENGTH
+
+    var query = PhysicsRayQueryParameters3D.create(from, to)
+    query.collide_with_areas = true
+
+    var result = space_state.intersect_ray(query)
+    if (result.is_empty()):
+      print("returning")
+      return
+
+    get_parent().get_node("MeshInstance3D").global_transform.origin = result.position
+    playerDir.transform.basis = basis.looking_at(result.position)
+
+  elif (direction.length() != 0):
     playerDir.transform.basis = basis.looking_at(direction)
