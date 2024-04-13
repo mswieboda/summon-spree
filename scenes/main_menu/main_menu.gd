dbc3922bec1c6f589ee4d7713bb56197c8b54ee2
Matt Swieboda <mswieboda@users.noreply.github.com>
2024-04-13 11:26:00 -0500

    games list menu

--- scenes/main_menu/main_menu.gd
+++ scenes/main_menu/main_menu.gd
@@ -1,23 +1,39 @@
 extends Control
 
-signal start()
-@onready var vbox_buttons = %vbox_buttons
+# TODO: add a game name param to start signal
+#signal play_game()
+var is_ready = false
 
 func _ready():
-  focus_button()
+  is_ready = true
+  focus_button($title_page)
+
 
 func _on_start_button_pressed():
-  start.emit()
-  hide()
+  $title_page.hide()
+  $games_page.show()
+  focus_button($games_page)
+
 
 func _on_exit_button_pressed():
   get_tree().quit()
 
-func _on_visibility_changed():
-  if visible:
-    focus_button()
 
-func focus_button():
+func _on_visibility_changed():
+  if not $title_page or not $games_page or not is_ready:
+    return
+
+  if visible:
+    var page: MarginContainer = $title_page if $title_page.visible else $games_page
+    focus_button(page)
+
+
+func focus_button(page: MarginContainer):
+  if page == $games_page:
+    page.get_node("vbox/game_list").select(0)
+
+  var vbox_buttons = page.get_node("vbox/vbox_buttons")
+
   if not vbox_buttons:
     return
 
@@ -25,3 +41,17 @@ func focus_button():
 
   if button is Button:
     button.grab_focus()
+
+
+func _on_back_button_pressed():
+  $games_page.hide()
+  $title_page.show()
+  focus_button($title_page)
+
+
+func _on_play_button_pressed():
+  var game_list: ItemList = $games_page.get_node("vbox/game_list")
+  var selected_game_indexes: PackedInt32Array = game_list.get_selected_items()
+  var selected_game = game_list.get_item_text(selected_game_indexes[0])
+  #play_game.emit()
+  print('>>> play game:', selected_game)
