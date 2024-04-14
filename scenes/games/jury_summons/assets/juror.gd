392ed15ba9d847897e91b2ec5ea2e04a671bcacb
Timsumdimsum <148920710+Timsumdimsum@users.noreply.github.com>
2024-04-14 00:12:43 -0500

    graphics for trials
    determine trial type and expected thingy

--- scenes/games/jury_summons/assets/juror.gd
+++ scenes/games/jury_summons/assets/juror.gd
@@ -3,12 +3,17 @@ extends Node2D
 var gender #1 = male, 0 = female
 var honesty
 var bias
+var candHeight #will be in mm
+var candHairColor #switch statement for colors
+var candWeight #apples
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 
   bias = randi_range(0,5)
   honesty = randi_range(0,5)
+  candHeight = randf_range(7,12) #in Bananas
+  candWeight = randf_range(6,20) #in Stone
 
 
   if randf_range(-1, 1) > 0:
@@ -20,7 +25,7 @@ func _ready():
     get_node("maleSprite").visible = false
     get_node("femaleSprite").visible = true
 
-  pass # Replace with function body.
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- scenes/games/jury_summons/jury_sum_main.gd
+++ scenes/games/jury_summons/jury_sum_main.gd
@@ -6,10 +6,8 @@ var jurors = []
 var candidate
 var playerTurn = true
 var aiTurn = false
-var clipStat1
-var clipStat2
-var clipStat3
-var clipStat4
+var trialType
+var decisionType
 var jurIndex = 0
 var buttonAction = true
 var jurClass = preload("res://scenes/games/jury_summons/assets/juror.tscn")
@@ -19,10 +17,10 @@ var jurClass = preload("res://scenes/games/jury_summons/assets/juror.tscn")
 func _ready():
 
   jurorRemain = 6
+  determineTrial()
+  biasVsHonesty()
+  $TrialValue.set_text("Expected " + decisionType + ": " + str(randi_range(5,10)))
   createJurorsList()
-
-  pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -30,17 +28,10 @@ func _process(delta):
     playerAction()
 
 
-
-
-
 func newJuror():
   var j1 = jurClass.instantiate()
   return j1
 
-  #if jurorRemain > 0:
-
-    #pass #add new juror
-  pass
 
 
 func createJurorsList():
@@ -51,12 +42,12 @@ func indJuror():
   if jurIndex >= 0 and jurIndex < 6:
     #jurors[jurIndex].add_child(newJuror())
     candidate.add_child(newJuror())
-    print_debug(candidate.get_child(0))
+    #print_debug(candidate.get_child(0))
 
 
 
 func moveJuror():
-  print_debug(candidate.get_child(0))
+  #print_debug(candidate.get_child(0))
   candidate.get_child(0).reparent(jurors[jurIndex],false)
   jurIndex+=1
   jurorRemain-=1
@@ -68,17 +59,38 @@ func playerAction():
 
 
 func aiAction():
-
   indJuror()
   if randi_range(-1, 1) > 0: #1 = yes, 0 = no
     moveJuror()
     playerTurn = true
     aiTurn = false
-    print("AI Selected Yes")
+    #print("AI Selected Yes")
   else:
     candidate.get_child(0).queue_free()
     $Timer.start(1)
-    print("AI Selected N0")
+    #print("AI Selected N0")
+
+func biasVsHonesty():
+  var BvsH = randi_range(1,2)
+
+  if BvsH == 1:
+    decisionType = "Bias"
+  else:
+    decisionType = "Honesty"
+
+func determineTrial():
+  trialType = randi_range(1,3)
+
+  match trialType:
+    1:
+      $TrialType.set_text("Trial: Petty Theft")
+    2:
+      $TrialType.set_text("Trial: Murder Charge")
+    3:
+      $TrialType.set_text("Trial: Assault Charge")
+
+
+  pass
 
 func _on_yes_button_pressed():
   if buttonAction == false:
